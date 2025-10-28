59c1a3a069bfc11ef10d1ef3ac41ff56a6faa926
boom2306 <mm2300604@tkh.edu.eg>
2025-10-28 10:55:39 +0300

    first commit

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const { app } = require('./index.js');
 
-// Start the server (no database connection)
-const PORT = 3000;
+const db_access = require('./db.js');
+const db = db_access.db;
 
-// Start listening on the specified port
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// Initialize database tables
+    db.run(db_access.createTripTable, (err) => {
+        if (err) console.log('Error creating trip table:', err.message);
+    });
+
+    db.run(db_access.createUserTable, (err) => {
+        if (err) console.log('Error creating trip table:', err.message);
+    });
+
+    // Start listening on the specified port
+    const PORT = 3000;
+    app.listen(PORT, () => {
+        console.log(Server is running on port ${PORT});
+    });
